7320d7da2a9b0b72545c939b009332e02621f626
pratik sonar <pratik.sonar@zingworks.in>
2021-10-22 18:31:53 +0530

    header-footer

---
+++ src/components/Footer/Footer.component.jsx
@@ -0,0 +1,14 @@
+import './Footer.style.scss';
+const Footer = () => {
+    return ( 
+        <div className="footer-container">
+            <ul>
+                <li><a href="#">Terms of use</a></li>
+                <li><a href="#">Privacy notice</a></li>
+                <li><a href="#">@Copyright-MAMBO-2021</a></li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default Footer;
---
+++ src/components/Navbar/Navbar.component.jsx
@@ -0,0 +1,20 @@
+import './Navbar.style.scss';
+const Navbar = () => {
+    return ( 
+        <div className="navbar-container">
+            <nav>
+                <div className="navbar-logo">
+                    <h3>MAMBO</h3>
+                </div>
+                <ul>
+                    <li><a href="#" id="active">Home</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Help</a></li>
+                    <li><a href="#" id="logout">Logout</a></li>
+                </ul>
+            </nav>
+        </div>
+     );
+}
+ 
+export default Navbar;
